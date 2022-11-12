5359c0c851c17548589299333dd2ab22b2a51010
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2022-11-13 00:26:16 +0100

    Type definitions have been updated.

--- julia/rnd-procedures.jl
+++ julia/rnd-procedures.jl
@@ -6,24 +6,18 @@ abstract type RestrictedRandomization end
 
 
 # Completely Randomized Design (like tossing a coin)
-# param reperesents probability of a coin (p).
-# E.g., for a fair coin, p = 0.5 
 struct CRD <: RestrictedRandomization 
-    param::Number
 end
 
 
 # Truncated Binomial Design
 # subjects are allocated acoring to CRD (tossing a coin ) until one of 
 # treatments gets enough number of subjects (half of the total sample size). 
-# Then, the rest of the subjects is is deterministically allocated to another 
+# Then, the rest of the subjects is deterministically allocated to another 
 # tretament. 
-# param1 represents probability of a coin (p).
-# param2 represents total sample size (nsbj) 
-# E.g., for a fair coin, p = 0.5 
+#   - `nsbj` represents a total sample size in a study. 
 struct TBD <: RestrictedRandomization 
-    param1::Number
-    param2::Int64
+    nsbj::Int64
 end
 
 
@@ -37,42 +31,42 @@ end
 
 
 # Random Allocation Rule (Rand)
-# An type of PBD, where block size equals to the total sample size.
-# param reperesents a block size
+# A type of PBD, where block size equals to the total sample size.
+#   - `nsbj` represents a total sample size in a study. 
 struct RAND <: RestrictedRandomization
-    param::Int64
+    nsbj::Int64
 end
 
 
 # Efron's Biased Coin Design
-# param represents a probability of a biased coin (p).
-# p = 2/3 is recommended
+#   - `param` represents a probability of a biased coin (p).
+#     p = 2/3 is recommended.
 struct EBCD <: RestrictedRandomization
     param::Number
 end
 
 
 # Adjustable Biased Coin Design
-# param represents a parameter (a) of a probability function used 
-# in treatment assignments. 
+#   - `param`represents a parameter (a) of a probability function used 
+#     in treatment assignments. 
 struct ABCD <: RestrictedRandomization
     param::Number
 end
 
 
 # Generalized Biased Coin Design
-# param represents a parameter (γ) of a probability function used 
-# in treatment assignments. 
+#   - `param` represents a parameter (γ) of a probability function used 
+#     in treatment assignments. 
 struct GBCD <: RestrictedRandomization
     param::Number
 end
 
 
 # Big Stick Design
-# param represents maximum tolerated imbalance (MTI), a parameter that 
-# controls treatment imbalance. Thus, if at some allocation step, an absolute 
-# treatment imbalance equals to MTI, then next allocation is deterministically 
-# forced toward balance.
+#   - `param`` represents maximum tolerated imbalance (MTI), a parameter that 
+#     controls treatment imbalance. Thus, if at some allocation step, an absolute 
+#     treatment imbalance equals to MTI, then next allocation is deterministically 
+#     forced toward balance.
 struct BSD <: RestrictedRandomization
     param::Int64
 end
@@ -80,8 +74,8 @@ end
 
 # Biased Coin Design With Imbalance Tolerance
 # A combination of Efron's Biased Coin Design and MTI procedure.
-# param1 represents a probability of BCD.
-# param2 represents a MTI parameter.
+#   - `param1` represents a probability of BCD.
+#   - `param2` represents a MTI parameter.
 struct BCDWIT <: RestrictedRandomization
     param1::Number
     param2::Int64
